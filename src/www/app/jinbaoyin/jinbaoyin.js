b510fbe4aad14bdbc16acab13c27f04eb4afe5ec
Siqi Lu <lu.siqi@outlook.com>
2015-06-11 19:58:08 +0800

    201506111958

--- src/www/app/jinbaoyin/jinbaoyin.js
+++ src/www/app/jinbaoyin/jinbaoyin.js
@@ -15,29 +15,17 @@ angular.module('jym.jinbaoyin', [
     })
     .controller('JinbaoyinCtrl', function(JinbaoyinService) {
         var jinbaoyinCtrl = this;
-        jinbaoyinCtrl.product = {
-            endSellTime: "06/30/2015 18:30:00",
-            financingSumAmount: 10000000,
-            issueNo: 0,
-            issueTime: "06/11/2015 18:07:58",
-            paidAmount: 0,
-            productCategory: 100000030,
-            productIdentifier: "530696882a1d4050814428c577dc4064",
-            productName: "金包银",
-            productNo: "JBY0001",
-            soldOut: false,
-            soldOutTime: "01/01/0001 00:00:00",
-            startSellTime: "06/11/2015 08:30:00",
-            unitPrice: 100000,
-            updateTime: "06/11/2015 18:07:59",
-            valueDateMode: 1,
-            'yield': 700
-        };
+        jinbaoyinCtrl.product = {};
 
         jinbaoyinCtrl.refreshProduct = function(){
             JinbaoyinService.getIndex()
                 .then(function(result) {
-                    jinbaoyinCtrl.product = result.data;
+                    return result.data;
+                })
+                .then(function(result) {
+                    jinbaoyinCtrl.product.title = result.productName + ' ' + '第' + result.issueNo + '期';
+                    jinbaoyinCtrl.product.yield = result.yield / 100;
+                    jinbaoyinCtrl.product.unitPrice = result.unitPrice / 100;
                 });
         };
 
