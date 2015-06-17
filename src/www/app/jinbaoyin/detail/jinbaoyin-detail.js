ca0aa7b0690370c18904c059420cd063c4ebeeba
Siqi Lu <lu.siqi@outlook.com>
2015-06-17 10:57:58 +0800

    201506171057

--- src/www/app/jinbaoyin/detail/jinbaoyin-detail.js
+++ src/www/app/jinbaoyin/detail/jinbaoyin-detail.js
@@ -52,45 +52,19 @@ angular.module('jym.jinbaoyin.detail', [
             }
         };
 
+        var getValueDateMode = function(valueDateMode) {
+            if(valueDateMode <= 0) {
+                return '购买成功立刻起息';
+            }
+
+            return '购买成功T+' + valueDateMode + '工作日起息';
+        };
+
         product.model = {};
         product.viewModel = {};
         product.viewModel.investCount = 10;
         product.viewModel.investAmount = 0;
         product.viewModel.expectedInterest = 0;
-
-        product.refreshInvestViewModel = function() {
-            if (isFinite(product.viewModel.investCount)) {
-                product.viewModel.investAmount = product.viewModel.investCount * product.viewModel.unitPrice;
-            } else {
-                product.viewModel.investAmount = 0;
-            }
-
-            product.viewModel.expectedInterest = (JYMProductService.getInterest(product.viewModel.investAmount * 100, product.model.yield, 30) / 100).toFixed(2);
-        };
-
-        product.refreshViewModel = function() {
-            this.viewModel.title = this.model.productName + ' ' + '第' + this.model.issueNo + '期';
-            this.viewModel.financingSumAmount = (this.model.financingSumAmount / 1000000).toFixed(0);
-            this.viewModel.productNo = this.model.productNo;
-            this.viewModel.remainCount = ((this.model.financingSumAmount - this.model.paidAmount) / this.model.unitPrice).toFixed(0);
-            this.viewModel.sellProgress = getSaleProgress(this.model);
-            this.viewModel.sellProgressInCircleProgress = this.viewModel.sellProgress / 100;
-            this.viewModel.status = getSaleStatus(this.model);
-            this.viewModel.yield = (this.model.yield / 100).toFixed(2);
-            this.viewModel.unitPrice = (this.model.unitPrice / 100).toFixed(0);
-        };
-
-        product.refreshProduct = function() {
-            JinbaoyinService.getIndex()
-                .then(function(result) {
-                    return result.data;
-                })
-                .then(function(result) {
-                    product.model = result;
-                    product.refreshViewModel();
-                    product.refreshInvestViewModel();
-                });
-        };
 
         product.doRefresh = function() {
             product.refreshProduct();
@@ -108,8 +82,44 @@ angular.module('jym.jinbaoyin.detail', [
             if (product.viewModel.investCount > product.viewModel.remainCount) {
                 product.viewModel.investCount = parseInt(product.viewModel.remainCount);
             }
-
+            
             product.refreshInvestViewModel();
+        };
+
+        product.refreshInvestViewModel = function() {
+            if (isFinite(product.viewModel.investCount)) {
+                product.viewModel.investAmount = product.viewModel.investCount * product.viewModel.unitPrice;
+            } else {
+                product.viewModel.investAmount = 0;
+            }
+
+            product.viewModel.expectedInterest = (JYMProductService.getInterest(product.viewModel.investAmount * 100, product.model.yield, 30) / 100).toFixed(2);
+        };
+
+        product.refreshProduct = function() {
+            JinbaoyinService.getIndex()
+                .then(function(result) {
+                    return result.data;
+                })
+                .then(function(result) {
+                    product.model = result;
+                    product.refreshViewModel();
+                    product.refreshInvestViewModel();
+                });
+        };
+
+        product.refreshViewModel = function() {
+            this.viewModel.issueNo = parseInt(this.model.issueNo);
+            this.viewModel.financingSumAmount = (this.model.financingSumAmount / 1000000).toFixed(0);
+            this.viewModel.productName = this.model.productName + ' ' + '第' + this.model.issueNo + '期';
+            this.viewModel.productNo = this.model.productNo;
+            this.viewModel.remainCount = ((this.model.financingSumAmount - this.model.paidAmount) / this.model.unitPrice).toFixed(0);
+            this.viewModel.sellProgress = getSaleProgress(this.model);
+            this.viewModel.sellProgressInCircleProgress = this.viewModel.sellProgress / 100;
+            this.viewModel.status = getSaleStatus(this.model);
+            this.viewModel.unitPrice = (this.model.unitPrice / 100).toFixed(0);
+            this.viewModel.valueDateMode = getValueDateMode(this.model.valueDateMode);
+            this.viewModel.yield = (this.model.yield / 100).toFixed(2);
         };
 
         product.doRefresh();
