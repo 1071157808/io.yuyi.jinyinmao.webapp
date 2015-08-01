488ad7f4e1dd665a18115be212d004207b03ceb3
Siqi Lu <lu.siqi@outlook.com>
2015-08-01 17:38:26 +0800

    13.1.49

--- src/www/app/common/constants.js
+++ src/www/app/common/constants.js
@@ -127,6 +127,6 @@ angular.module('jym.constants', [])
         URL: /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[:?\d]*)\S*$/
     })
     .constant('APP', {
-        VERSION: '13.1.48-DEV',
+        VERSION: '13.1.49-DEV',
         ENV: 'dev'
     });
