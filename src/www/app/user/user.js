a38e1eb7318154508392e65de37ea5a356c71e00
keitei <keitei@live.com>
2015-08-14 11:08:57 +0800

    201508141108

--- src/www/app/user/user.js
+++ src/www/app/user/user.js
@@ -32,8 +32,7 @@ angular.module('jym.user', [
 
         ctrl.viewModel.showQian1 = false;
         ctrl.viewModel.showQian2 = false;
-        //ctrl.viewModel.qian = false;
-        ctrl.viewModel.amount = "0.00";
+        ctrl.viewModel.amount = '0.00';
 
         ctrl.doRefresh = function() {
             ctrl.refreshUser()
